f45ea76d075f7bc47fa4aa2a80147e8b524203f8
xiaomaolin <xiaomaolin@meituan.com>
2019-03-24 19:27:30 +0800

    初版promise

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Promise = require('./promise');
 
-new Promise((resolve, reject) => {
-  resolve(123);
-}).then(data => {
+let _resolve;
+let promise = new Promise((resolve, reject) => {
+  _resolve = resolve;
+})
+
+promise.then(data => {
   console.log('data: ', data)
 }, err => {
   console.log('error: ', err)
 })
+
+_resolve(123);
--- promise.js
+++ promise.js
@@ -1,5 +1,66 @@
-function Promise() {
+function execCallback(promise) {
+  const defers = promise.defers;
+  while (defers.length > 0) {
+    const defer = defers.shift();
+    const nextPromise = defer.promise;
+    const callback = promise.state === 'fullfilled' ?  defer.onFullfilled : defer.onRejected;
+    let cbRes;
+    try {
+      cbRes = callback(promise.data);
+    } catch (err) {
+      reject(nextPromise, err);
+      continue;
+    }
+    if (cbRes instanceof Promise) {
+      cbRes.then(data => {          // 当cbRes也是Promise时，保证nextPromise的状态与cbRes一致
+        resolve(nextPromise, data);
+      }, err => {
+        reject(nextPromise, err);
+      });
+    } else {
+      resolve(nextPromise);
+    }
+  }
+}
 
+function resolve(promise, data) {
+  promise.data = data;
+  promise.state = 'fullfilled';
+  execCallback(promise);
+}
+
+function reject(promise, err) {
+  promise.data = err;
+  promise.state = 'rejected';
+  execCallback(promise);
+}
+
+function Promise(fn) {
+  this.state = 'pending';    // pending|fullfilled|rejected
+  this.data = undefined;
+  this.defers = [];     // 保存 callback 和 nextPromise
+
+  const promise = this;
+  fn(data => {
+    resolve(promise, data);
+  }, err => {
+    reject(promise, err);
+  });
+  return this;
 };
+
+Promise.prototype.then = function(onFullfilled, onRejected) {
+  const nextPromise = new Promise(function() {});
+  let defer = {
+    promise: nextPromise,
+    onFullfilled,
+    onRejected
+  };   // 回调的执行会对nextPromise产生影响，故一起保存
+  this.defers.push(defer)
+  if (this.state !== 'pending') {
+    execCallback(this);    // 非pending状态，触发callback
+  }
+  return nextPromise;
+}
 
 module.exports =  Promise;
